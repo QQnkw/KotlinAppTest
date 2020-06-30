cd1608b79765831b217121702ee4876776d375ca
QQnkw <40155899+QQnkw@users.noreply.github.com>
2020-06-30 19:05:15 +0800

    视图绑定和数据绑定

---
+++ app/src/main/java/com/kotlin/kotlinapp/jetpack/ViewBindingActivity.kt
@@ -0,0 +1,18 @@
+package com.kotlin.kotlinapp.jetpack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kotlin.kotlinapp.databinding.ActivityViewBindingBinding
+
+/**
+ * 视图绑定activity
+ */
+class ViewBindingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityViewBindingBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityViewBindingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.textView.text = "123"
+    }
+}
