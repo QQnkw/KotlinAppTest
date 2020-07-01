ef83f3af8eada72ecb5e1f6506ecc76c9913d127
QQnkw <40155899+QQnkw@users.noreply.github.com>
2020-07-01 18:42:07 +0800

    viewmodel简单使用2

--- app/src/main/java/com/kotlin/kotlinapp/jetpack/viewmodel/FirstViewModel.kt
+++ app/src/main/java/com/kotlin/kotlinapp/jetpack/viewmodel/FirstViewModel.kt
@@ -1,9 +1,8 @@
 package com.kotlin.kotlinapp.jetpack.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 import com.kotlin.kotlinapp.bean.User
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -21,5 +20,16 @@ class FirstViewModel : ViewModel() {
 
     private fun loadUsers() {
         //异步操作获取用户
+    }
+
+    private val user: LiveData<User> = liveData {
+//        emit()//发送
+    }
+
+    private val userId: LiveData<String> = MutableLiveData()
+    private val otherUser = userId.switchMap { userId: String ->
+        liveData(context = viewModelScope.coroutineContext + Dispatchers.IO) {
+            emit(User())
+        }
     }
 }
